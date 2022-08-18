eca84cb54bd8a7f927012b40c95addb49d4e2ac4
Manjot_Sarna <manjotsarna44@gmail.com>
2022-08-18 19:14:21 -0400

    Added Employee and Department Models

---
+++ ASP_FinalExam_Net6/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Xunit.Sdk;
+
+namespace ASP_FinalExam_Net6.Models
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "A Department Name is Required")]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Range(0,int.MaxValue)]
+        public int EmployeeCount { get; set; }
+    }
+}
---
+++ ASP_FinalExam_Net6/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Xunit.Sdk;
+
+namespace ASP_FinalExam_Net6.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "An Employee Name is Required")]
+        [StringLength(100)]
+        public string Name { get; set; }
+        public bool isManager { get; set; }
+        [ForeignKey("Department")]
+        public int DepartmentId { get; set; }
+    }
+}
